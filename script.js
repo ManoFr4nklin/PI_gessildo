af53d5fb08d555a99342588373d91947545dc852
ManoFr4nklin <79010620+ManoFr4nklin@users.noreply.github.com>
2024-10-14 20:46:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById('loginForm');
+const mensagem = document.getElementById('mensagem');
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
+
+  // Simulando a verificação de credenciais (substitua pela sua lógica real)
+  if (email === 'admin@gmail.com' && senha === '123456') {
+    // Credenciais válidas, redirecionar para outra página
+    window.location.href = '/Pi-Mint/tela_prp/tela_princ.html';
+  } else {
+    mensagem.textContent = 'Usuário ou senha inválidos.';
+  }
+});
